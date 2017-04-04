50d8eb7df4fae02af8baa6345bf7f87ab2a3414b
Szilard Magyar <szilard.magyar@gmail.com>
2017-04-05 00:01:41 +0200

    add algorithms

---
+++ algorithms/lib/binary_search.rb
@@ -0,0 +1,40 @@
+class BinarySearch
+  #binary_serch with recursion
+  def binary_search(array, value, from = 0, to = nil)
+    to = array.count - 1 unless to
+
+    return "Value not in array" if from > to
+
+    mid = (from + to) / 2
+
+    if value < array[mid]
+      return binary_search(array, value, from, mid - 1)
+    elsif value > array[mid]
+      return binary_search(array, value, mid + 1, to)
+    else
+      return mid
+    end
+  end
+
+  #binary search without recursion
+  def binary_search_with_while(array, value)
+    from = 0
+    to = array.count - 1
+
+    while from <= to do
+      mid = (from + to) / 2
+
+      if value < array[mid]
+        to = array[mid] - 1
+      elsif value > array[mid]
+        from = array[mid] + 1
+      else
+        return mid
+      end
+    end
+
+    return "Value not in array"
+  end
+end
+
+puts BinarySearch.new.binary_search([10, 20, 30, 40, 50], 60)
---
+++ algorithms/spec/binary_search_spec.rb
@@ -0,0 +1,18 @@
+require 'binary_search'
+require 'byebug'
+
+describe BinarySearch do
+  let(:search_instance) { BinarySearch.new }
+
+  context '#binary_search' do
+    it 'returns the index if can be found' do
+      expect(search_instance.binary_search([10, 20, 30, 40, 50], 40)).to eq(3)
+      expect(search_instance.binary_search([10, 20, 30, 40, 50], 10)).to eq(0)
+    end
+
+    it 'returns the error message if value not in array' do
+      expect(search_instance.binary_search([10, 20, 30, 40, 50], 60)).to eq("Value not in array")
+      expect(search_instance.binary_search([10, 20, 30, 40, 50], -5)).to eq("Value not in array")
+    end
+  end
+end
