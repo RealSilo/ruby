4af2055f62c96137f36b7e4759130116998f60f6
szilard-devolute <szilard@devolute.org>
2017-05-03 16:11:46 +0200

    html dsl and ruby object model

---
+++ html_dsl.rb
@@ -0,0 +1,107 @@
+class HTML
+  def initialize(&block)
+    @rendered = ''
+    instance_eval(&block)
+  end
+
+  def h1 content
+    @rendered << "<h1>#{content}</h1>"
+  end
+
+  def div content
+    @rendered << "<div>#{content}</div>"
+  end
+
+  def render
+    @rendered
+  end
+end
+
+result = HTML.new do
+  h1 'Dev meeting'
+
+  div 'hi there'
+end
+
+# p result.render
+
+class HTML2
+  TAG_LIST = %i(h1 div span ul li)
+
+  def initialize(&block)
+    @rendered = ''
+    instance_eval(&block)
+  end
+
+  # def h1(content, &block)
+  #   if block_given?
+  #     @rendered << "<h1>"
+  #     block.call if block_given?
+  #   else
+  #     @rendered << "<h1>#{content}"
+  #   end
+  #   @rendered << "</h1>"
+  # end
+
+  # def span content
+  #   @rendered << "<span>#{content}</span>"
+  # end
+
+  # def div content
+  #   @rendered << "<div>#{content}</div>"
+  # end
+
+  # def ul(&block)
+  #   @rendered << "<ul>"
+  #   block.call if block_given?
+  #   @rendered << "</ul>"
+  # end
+
+  # def li(content, &block)
+  #   if block_given?
+  #     @rendered << "<li>"
+  #     block.call if block_given?
+  #   else
+  #     @rendered << "<li>#{content}"
+  #   end
+  #   @rendered << "</li>"
+  # end
+
+  # def method_missing(tag, content)
+  #   @rendered << "<#{tag}>#{content}</#{tag}>"
+  # end
+
+  TAG_LIST.each do |tag|
+    define_method(tag) do |content = '', options = {}, &block|
+      attributes = options.reduce('') { |result, (key, val)| result << "#{key}='#{val}' " }
+      if block
+        @rendered << "<#{tag} #{attributes}>"
+        block.call
+      else
+        @rendered << "<#{tag} #{attributes}>#{content}"
+      end
+      @rendered << "</#{tag}>"
+    end
+  end
+
+  def render
+    @rendered
+  end
+end
+
+result2 = HTML2.new do
+  h1 'Dev meeting' do
+    span 'hahaha'
+  end
+
+  div 'hi there'
+
+  ul do
+    li 'list element', class: 'haha', id: 'hihi'
+    li 'list element' do
+      span 'alfsadlf'
+    end
+  end
+end
+
+p result2.render
---
+++ ruby_object_model.rb
@@ -0,0 +1,51 @@
+class A
+end
+
+class B < A
+  def self.baz
+  end
+
+  # class << self
+  #   def hihi
+  #   end
+  # end
+end
+
+def B.baz
+end
+
+b = B.new
+
+def haha
+  puts "hihi"
+end
+
+# BasicObject.send(:haha)
+# Object.send(:haha)
+# self.send(:haha)
+# puts self
+# puts self.class
+# puts self.to_s
+
+# puts A.superclass
+# puts B.class
+# puts B.superclass.singleton_class
+# puts B.singleton_class.superclass
+# puts b.singleton_class
+# puts b.superclass
+# puts b.singleton_class.superclass
+# puts b.singleton_class.object_id
+# puts b.object_id
+# puts B.singleton_class.object_id
+# puts B.object_id
+# puts B.singleton_class.superclass
+
+# puts A.superclass.ancestors
+# puts BasicObject.class
+# puts BasicObject.superclass
+# puts Class.class
+# puts Class.superclass
+# puts Module.class
+# puts Module.superclass
+# puts BasicObject.superclass.class
+# puts Class.superclass.superclass.superclass.superclass.class.class
