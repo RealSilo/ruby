bd381c4bec19b942fb47d3582129cf2b5b64dc9c
Szilard Magyar <szilard.magyar@gmail.com>
2017-08-22 18:08:23 +0200

    add common longest substring and subsequence

--- lib/algorithms/dynamic.rb
+++ lib/algorithms/dynamic.rb
@@ -36,5 +36,81 @@ class Dynamic
   #   arr[amount]
   # end
 
-  # PROBLEM 3
+  # PROBLEM3: Common longest subsequence
+  # 'abcdegfgh' and 'aqcezfg' => 'acefg'
+
+  def common_longest_subsequence(s1, s2)
+    return 0 if s1.empty? || s2.empty?
+
+    num = Array.new(s1.size) { Array.new(s2.size) }
+
+    chars  = []
+
+    s1.each_char.with_index do |letter1, i|
+      s2.each_char.with_index do |letter2, j|
+        if letter1 == letter2
+          if i == 0 || j == 0
+            num[i][j] = 1
+          else
+            num[i][j] = 1 + num[i - 1][j - 1]
+            chars << [letter1, i, j] unless chars.any? { |char| char[1] > i || char[2] > j }
+          end
+        else
+          if i == 0 && j == 0
+             num[i][j] = 0
+          elsif i == 0 &&  j > 0  #First ith element
+             num[i][j] = [0,  num[i][j - 1]].max
+          elsif j == 0 && i > 0  #First jth element
+              num[i][j] = [0, num[i - 1][j]].max
+          else
+            num[i][j] = [num[i - 1][j], num[i][j - 1]].max
+          end
+        end
+      end
+    end
+    
+    p num[s1.length - 1][s2.length - 1]
+    chars.map(&:first).join
+  end
+
+  # PROBLEM3: Common longest substring
+  # 'abcdegfgh' and 'aqcezfg' => 'fg'
+  # O(MN) time and O(MN) complexity
+  # Only the previous column of the grid storing the dynamic state is ever 
+  # actually used in computing the next column. Thus, these algorithm can be
+  # altered to have only an O(N) storage requirement. By reassigning array
+  # references between two 1D arrays, this can be done without copying the state
+  # data from one array to another.
+  # Suffix trees (not trie) can be used to achieve a O(N+M) run time at the cost
+  # of extra storage and algorithmic complexity.
+  def common_longest_substring(str1, str2)
+    return '' if str1.empty? || str2.empty?
+    
+    array = Array.new(str1.length) { Array.new(str2.length) { 0 } }
+    intersection_length = 0
+    intersection_end    = 0
+    
+    str1.length.times do |x|
+      str2.length.times do |y|
+        # Go to next if letters are not the same
+        next unless str1[x] == str2[y]
+        if x == 0 && y == 0
+          array[x][y] = 1
+        else
+          # If letters are the same add 1 to the previous letter's number
+          array[x][y] = 1 + array[x - 1][y - 1]
+        end
+        # Go to next if length not greater than max
+        next unless array[x][y] > intersection_length
+        intersection_length = array[x][y]
+        intersection_end = x
+      end
+    end
+    intersection_start = intersection_end - intersection_length + 1
+
+    str1.slice(intersection_start..intersection_end)
+  end
 end
+
+puts Dynamic.new.common_longest_subsequence('houseboat', 'computer')
+puts Dynamic.new.common_longest_substring('houseboat', 'coumputer')
